b6295ce25555e9b2408a5c15b3cfd8b8f193a82b
nithinkumar2468 <nithinkumarkodipyaka@gmail.com>
2024-01-08 18:47:24 +0530

    commit4

--- src/main/java/com/springbootprofiling/SpringBootProfilingApplication.java
+++ src/main/java/com/springbootprofiling/SpringBootProfilingApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SpringBootProfilingApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringBootProfilingApplication.class, args);
 	}
 
